a24dadba16caf2dbbea5eb8acd9be042f5a9db82
Martin-bot-man <martinwere14@gmail.com>
2025-10-25 08:43:57 +0300

    Add schema.sql for 3NF database schema in Django project

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE if NOT EXISTS users (
 );
 CREATE TABLE if NOT EXISTS properties(
     property_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    owner_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
+    host_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
     name VARCHAR(100) NOT NULL,
     description TEXT NOT NULL,
     location VARCHAR(255) NOT NULL,
@@ -21,3 +21,29 @@ CREATE TABLE if NOT EXISTS properties(
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
 );
+CREATE TABLE if NOT EXISTS bookings(
+    booking_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    property UUID REFERENCES properties(property_id ) ON DELETE CASCADE,
+    guest_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL(10, 2) NOT NULL,
+    status ENUM ('pending', 'confirmed', 'cancelled') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+CREATE TABLE if NOT EXISTS reviews(
+    review_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    property_id UUID REFERENCES properties(property_id) ON DELETE CASCADE,
+    user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
+    rating INT CHECK (rating >=1 AND rating <=5) NOT NULL,
+    comment TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+);
+CREATE TABLE if NOT EXISTS messages(
+    message_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    sender_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
+    recepient_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
+    message_body TEXT NOT NULL,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
