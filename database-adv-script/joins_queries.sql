ebe42191a2da0bb90bd7a1f03b81f685684612a7
Martin-bot-man <martinwere14@gmail.com>
2025-10-28 20:03:42 +0300

    feat(queries): add INNER JOIN query for reviews details

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -13,3 +13,14 @@ SELECT
 FROM bookings b
 INNER JOIN users u on b.guest_id = u.user_id
 ORDER BY b.created_at DESC
+
+SELECT 
+    p.property_id,
+    p.price_per_night
+    p.created_at AS property_created_at,
+    r.review_id,
+    r.rating,
+    r.comment,
+    r.created_at AS review_created_at
+FROM properties p
+OUTER JOIN reviews r ON p.property_id = r.property_id
