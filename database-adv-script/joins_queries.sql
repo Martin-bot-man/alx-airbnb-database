9edc18a0e4b1d746a5f946dbf040ac78cec6d458
Martin-bot-man <martinwere14@gmail.com>
2025-11-04 03:33:46 +0300

    feat: add subquery for high-rated properties (rating > 4.0)

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -102,28 +102,7 @@ GROUP BY
 ORDER BY 
  average_rating DESC NULLS LAST, total_reviews DESC;   
 
-SELECT
- p.property_id,
- p.name AS property_name,
- p.description,
- p.location,
- p.price_per_night,
- p.created_at
-FROM
- properties P
-WHERE
- p.property_id IN(
-    SELECT 
-        r.property_id
-    FROM 
-        reviews r
-    GROUP BY
-        r.property_id
-    HAVING AVG(r.rating)> 4.0
 
- ) 
- ORDER BY
-    p.name;
      
 
 
