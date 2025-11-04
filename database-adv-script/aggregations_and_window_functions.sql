70f14af98791fc5e26ec62d7392e5339957b4005
Martin-bot-man <martinwere14@gmail.com>
2025-11-05 02:32:36 +0300

    feat: add GROUP BY query to rank properties based on bookings received

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -12,3 +12,19 @@ GROUP BY
  u.user_id, u.first_name, u.last_name,u.email
 ORDER BY
  total_bookings DESC, u.first_name;
+
+ SELECT 
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    p.price_per_night,
+    COUNT(b.booking_id)AS total_bookings,
+    ROW_NUMBER() OVER(ORDER BY COUNT(b.booking_id)DESC)AS row_number_rank)
+ FROM
+    properties P
+ LEFT JOIN
+    bookings b ON p.property_id = b.property
+ GROUP BY
+    p.property_id, p.name, p.location, p.price_per_night
+ ORDER BY
+    row_number_rank;            
